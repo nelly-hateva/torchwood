8eadbd5d1d4a32e7b0b99147e7609171cd529791
Nelly Hateva <nelly.hateva@gmail.com>
2014-02-15 15:27:39 +0200

    clean the corpus

---
+++ parser.py
@@ -0,0 +1,43 @@
+import glob
+import os
+import re
+import string
+from bs4 import BeautifulSoup
+from stemmer import PorterStemmer
+
+
+here = lambda *x: os.path.abspath(os.path.join(os.path.dirname(__file__), *x))
+data_dir=here('data', )
+corpus_dir=here('corpus', )
+
+total_files = 0
+exclude = set(string.punctuation)
+
+if not os.path.exists(corpus_dir):
+    os.makedirs(corpus_dir)
+os.chdir(data_dir)
+
+for file in glob.glob('*.sgm'):
+    current_file = os.path.join(data_dir, file)
+    print 'Extract files from file %s' % current_file
+    soup = BeautifulSoup(open(current_file))
+    for document in soup.find_all('reuters'):
+        new_file = os.path.join(corpus_dir, document.get('newid'))
+        with open(new_file, "wb") as extracted_file:
+            read_data = document.get_text().encode('utf-8')
+            clean_data = re.sub(r'/', ' / ', read_data)
+            clean_data = re.sub(r'-', ' - ', clean_data)
+            """
+            The punctuations contained in the 'string.punctuation are 
+            " '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' "
+            """
+            clean_data = read_data.translate(None, string.punctuation)
+            clean_data = clean_data.lower()
+            p = PorterStemmer()
+            output = ''
+            for word in clean_data.split():
+                output += p.stem(word, 0, len(word) - 1)
+                output += ' '
+            extracted_file.write(output)
+            total_files += 1
+print 'Total files extracted %s' % total_files
---
+++ special_characters.py
@@ -0,0 +1,27 @@
+import glob
+import os
+import re
+
+here = lambda *x: os.path.abspath(os.path.join(os.path.dirname(__file__), *x))
+reuters_dir=here('reuters21578', )
+data_dir=here('data', )
+
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+os.chdir(reuters_dir)
+for file in glob.glob('*.sgm'):
+    current_file = os.path.join(reuters_dir, file)
+    print 'Removing special characters from file %s' % current_file
+    with open(current_file, 'r') as f:
+        read_data = f.read()
+        # 00 - 47; 123 - 159 punctuation unused
+        clean_data = re.sub(r'&#((\d|[0-3]\d)|(4[0-7]));', '', read_data)
+        clean_data = re.sub(r'&#(1(2[3-9]|[3-4]\d|5[0-9]));', '', clean_data)
+        # < less-than sign, it will be removed anyway
+        clean_data = re.sub(r'&lt;', '', clean_data)
+        # remove f****reute that appears in unknown tag
+        clean_data = re.sub(r'f(\d{,4})reute', '', clean_data)
+        new_file = os.path.join(data_dir, file)
+        with open(new_file, 'w+') as nf:
+            nf.write(clean_data)
